30f2cecc059ef830220e18b999cd39007191f4e7
radek.Bednarik <radek.bednarik@tesena.com>
2024-11-27 14:57:22 +0100

    dev: added image and got them working

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import './style.css'
+import imagePoutyTrump from './images/trump_mug_pouty.png'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="text-red-900">
     Yo, hello world! 
+    <img id="trump_image" class="rounded-lg"/>
   </div>
 `
 
+const trumpImage = document.querySelector<HTMLImageElement>('#trump_image')
+if (trumpImage) {
+  trumpImage.src = imagePoutyTrump
+}
