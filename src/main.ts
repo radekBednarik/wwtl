954d970bcbe6686f489d0b2010f3dce2875dc9a9
radek.Bednarik <radek.bednarik@tesena.com>
2024-11-28 12:37:24 +0100

    dev: wip

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import './style.css'
 import imagePoutyTrump from './images/trump_mug_pouty.png'
+import { formatDistanceToNow } from 'date-fns'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="text-red-900">
-    Yo, hello world! 
-    <img id="trump_image" class="rounded-lg"/>
+  <div id='body' class="container flex flex-col mx-auto align-middle">
+    <h1 class="2xl:text-9xl xl:text-8xl md:text-4xl sm: text-xl mx-auto mb-6">When will Trump leave?</h1>
+    <img id="trump_image" class="mx-auto rounded-lg 2xl:size-fit xl:size-fit md:size-96 sm:size-64 mb-6"/>
+    <div class="mx-auto 2xl:text-6xl xl:text-6xl md:text-4xl sm: text-xl mb-6">Well, hopefully in</div>
   </div>
 `
 
@@ -12,3 +14,32 @@ const trumpImage = document.querySelector<HTMLImageElement>('#trump_image')
 if (trumpImage) {
   trumpImage.src = imagePoutyTrump
 }
+
+injectCountdown()
+
+function injectCountdown() {
+  const timeTarget = new Date(2028, 11, 7)
+  const countdownElement = document.createElement('div')
+  countdownElement.id = 'countdown'
+  countdownElement.classList.add('mx-auto')
+
+  setInterval((countdownElement: HTMLDivElement, timeTarget: number) => {
+    const distance = formatDistanceToNow(timeTarget, { includeSeconds: true })
+    const previousTimeDiv = document.querySelector<HTMLDivElement>('#time')
+
+    if (previousTimeDiv) {
+      countdownElement.removeChild(previousTimeDiv)
+    }
+
+    const timeDiv = document.createElement('div')
+    timeDiv.id = "time"
+    timeDiv.innerText = distance
+
+    countdownElement.appendChild(timeDiv)
+    document.querySelector<HTMLDivElement>('#body')!.appendChild(countdownElement!)
+
+
+  }, 1000, countdownElement, timeTarget)
+}
+
+
